4a49a75e550a7788ba20fbcb856ca6597c166daa
altaiirdesmond <altaiirdesmond@gmail.com>
2019-03-17 22:56:58 +0800

    optimization

--- Assets/Script/SearchManager.cs
+++ Assets/Script/SearchManager.cs
@@ -1,66 +1,75 @@
-﻿using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+﻿namespace Assets.Script {
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class SearchManager : MonoBehaviour {
+    /// <summary>
+    /// Defines the <see cref="SearchManager" />
+    /// </summary>
+    public class SearchManager : MonoBehaviour {
+        /// <summary>
+        /// Defines the databaseManager
+        /// </summary>
+        private DatabaseManager databaseManager;
 
-    private DatabaseManager databaseManager;
-    private Transform[] transforms;
+        /// <summary>
+        /// Defines the prefabHolder which holds result prefabs
+        /// </summary>
+        private Transform[] prefabHolder;
 
-    public static string Filter { get; set; }
+        /// <summary>
+        /// What food to be search
+        /// </summary>
+        /// <param name="inputField">The food to search</param>
+        public void SearchFood(TMP_InputField inputField) {
+            // resultPrefab will hold the image and text of a food instance
+            CategorySceneManager resultPrefab = GameObject.Find("SceneManager").GetComponent<CategorySceneManager>();
 
-    public void SearchFood(TMP_InputField inputField) {
-        // resultPrefab will hold the image and text of a food instance
-        CategorySceneManager resultPrefab = GameObject.Find("SceneManager").GetComponent<CategorySceneManager>();
+            prefabHolder = new Transform[resultPrefab.Panels[0].transform.childCount];
 
-        transforms = new Transform[resultPrefab.Panels[0].transform.childCount];
+            // Reset visibility
+            for (var i = 0; i < resultPrefab.Panels[0].transform.childCount; i++) {
+                resultPrefab.Panels[0].transform.GetChild(i).gameObject.SetActive(true);
+                prefabHolder[i] = resultPrefab.Panels[0].transform.GetChild(i);
+            }
 
-        // Reset visibility
-        for (int i = 0; i < resultPrefab.Panels[0].transform.childCount; i++) {
-            resultPrefab.Panels[0].transform.GetChild(i).gameObject.SetActive(true);
-            transforms[i] = resultPrefab.Panels[0].transform.GetChild(i);
-        }
+            if (inputField.text == "") {
+                return;
+            }
 
-        if (inputField.text == "") {
-            return;
-        }
-
-        // Show only the queried item.
-        foreach (Transform transform in transforms) {
-            if(Filter == "name") {
-                transform.GetChild(1).gameObject.SetActive(true);
-                transform.GetChild(2).gameObject.SetActive(false);
-                if (transform.GetChild(1).GetComponent<TextMeshProUGUI>().text.StartsWith(inputField.text)) {
-                    if (transform.gameObject.activeSelf == false) {
-                        transform.gameObject.SetActive(true);
+            // Show only the queried item.
+            foreach (var tf in prefabHolder) {
+                tf.GetChild(1).gameObject.SetActive(true);
+                tf.GetChild(2).gameObject.SetActive(false);
+                if (tf.GetChild(1).GetComponent<TextMeshProUGUI>().text.StartsWith(inputField.text) ||
+                    tf.GetChild(2).GetComponent<TextMeshProUGUI>().text.StartsWith(inputField.text)) {
+                    if (tf.gameObject.activeSelf == false) {
+                        tf.gameObject.SetActive(true);
                     }
                     continue;
                 }
-                transform.gameObject.SetActive(false);
-            } else {
-                transform.GetChild(1).gameObject.SetActive(false);
-                transform.GetChild(2).gameObject.SetActive(true);
-                if (transform.GetChild(2).GetComponent<TextMeshProUGUI>().text.StartsWith(inputField.text)) {
-                    if (transform.gameObject.activeSelf == false) {
-                        transform.gameObject.SetActive(true);
-                    }
-                    continue;
+
+                if (tf.GetChild(2).GetComponent<TextMeshProUGUI>().text.StartsWith(inputField.text)) {
+
                 }
-                transform.gameObject.SetActive(false);
+                tf.gameObject.SetActive(false);
             }
         }
-    }
 
-    public void FilterBy(string filter) {
-        Filter = filter;
-        Debug.Log("Filter changed to " + filter);
-    }
+        /// <summary>
+        /// Set which check image should be disabled.
+        /// </summary>
+        /// <param name="image">The image<see cref="Image"/></param>
+        public void SetDisable(Image image) {
+            image.gameObject.SetActive(false);
+        }
 
-    public void SetDisable(Image image) {
-        image.gameObject.SetActive(false);
-    }
-
-    public void SetEnable(Image image) {
-        image.gameObject.SetActive(true);
+        /// <summary>
+        /// Set which check image should be enabled.
+        /// </summary>
+        /// <param name="image">The image<see cref="Image"/></param>
+        public void SetEnable(Image image) {
+            image.gameObject.SetActive(true);
+        }
     }
 }
