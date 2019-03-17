2f905ffa267199720b38a76d9f1998bfcf41b3cb
altaiirdesmond <altaiirdesmond@gmail.com>
2019-03-17 22:53:57 +0800

    namespace changes

--- Assets/Script/Back.cs
+++ Assets/Script/Back.cs
@@ -1,27 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Back : MonoBehaviour {
+namespace Assets.Script {
+    public class Back : MonoBehaviour {
 
-    private static Back back;
+        private static Back back;
 
-    private void Awake() {
-        if(back != null) {
-            Destroy(gameObject);
-        } else {
-            back = this;
+        private void Awake() {
+            if(back != null) {
+                Destroy(gameObject);
+            } else {
+                back = this;
+            }
+
+            DontDestroyOnLoad(gameObject);
         }
 
-        DontDestroyOnLoad(gameObject);
-    }
-
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (SceneManager.GetActiveScene().buildIndex > 0) {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-            } else if (SceneManager.GetActiveScene().buildIndex == 0) {
-                FindObjectOfType<MenuManager>().ShowBlockingPanel();
-                FindObjectOfType<MenuManager>().ShowQuit();
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                if (SceneManager.GetActiveScene().buildIndex > 0) {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                } else if (SceneManager.GetActiveScene().buildIndex == 0) {
+                    FindObjectOfType<MenuManager>().ShowBlockingPanel();
+                    FindObjectOfType<MenuManager>().ShowQuit();
+                }
             }
         }
     }
