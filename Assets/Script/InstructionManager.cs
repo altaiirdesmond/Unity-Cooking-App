76e14fc7e25af2eac7ab0fc302e5855d936e4c46
altaiirdesmond <altaiirdesmond@gmail.com>
2019-03-17 22:55:55 +0800

    refactor

--- Assets/Script/InstructionManager.cs
+++ Assets/Script/InstructionManager.cs
@@ -1,33 +1,35 @@
-﻿using Assets.Script.DatabaseModel;
-using System.Linq;
+﻿using System.Linq;
+using Assets.Script.DatabaseModel;
 using TMPro;
 using UnityEngine;
 
-public class InstructionManager : MonoBehaviour {
+namespace Assets.Script {
+    public class InstructionManager : MonoBehaviour {
 
-    private TextMeshProUGUI proUGUI;
-    private string instructions;
+        private TextMeshProUGUI ingredientText;
+        private string instructions;
 
-    private void Start() {
-        Food food = MenuManager.Food;
+        private void Start() {
+            Food food = MenuManager.Food;
 
-        // Display the instructions on the ScrollRect
-        if (Lean.Localization.LeanLocalization.CurrentLanguage.Equals("English")) {
-            // Clean instruction before starting CookingScene
-            foreach (var item in food.InstructionTranslated.Split('\n')) {
-                string newText = item.Replace("{skip}", string.Empty);
-                foreach (var waitTime in newText.Split(' ').ToList().Where(i => i.StartsWith("WAIT_TIME:"))) {
-                    newText = newText.Replace(waitTime, string.Empty);
+            // Display the instructions on the ScrollRect
+            if (Lean.Localization.LeanLocalization.CurrentLanguage.Equals("English")) {
+                // Clean instruction before starting CookingScene
+                foreach (var item in food.InstructionTranslated.Split('\n')) {
+                    string newText = item.Replace("{skip}", string.Empty);
+                    foreach (var waitTime in newText.Split(' ').ToList().Where(i => i.StartsWith("WAIT_TIME:"))) {
+                        newText = newText.Replace(waitTime, string.Empty);
+                    }
+
+                    instructions += newText + "\n";
                 }
-
-                instructions += newText + "\n";
+                GetComponent<TextMeshProUGUI>().SetText(instructions);
+            } else {
+                foreach (var item in food.Instruction.Split('\n')) {
+                    instructions += item + "\n";
+                }
+                GetComponent<TextMeshProUGUI>().SetText(instructions);
             }
-            GetComponent<TextMeshProUGUI>().SetText(instructions);
-        } else {
-            foreach (var item in food.Instruction.Split('\n')) {
-                instructions += item + "\n";
-            }
-            GetComponent<TextMeshProUGUI>().SetText(instructions);
         }
     }
 }
